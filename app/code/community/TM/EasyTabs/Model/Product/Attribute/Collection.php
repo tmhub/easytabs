ed28d58d90c699fca44361dc5c239eb17248b8ab
0m3r <0m3r.mail@gmail.com>
2013-07-25 12:34:28 +0300

    attr widget 1.5 compatability was added (#57)

--- app/code/community/TM/EasyTabs/Model/Product/Attribute/Collection.php
+++ app/code/community/TM/EasyTabs/Model/Product/Attribute/Collection.php
@@ -1,10 +1,22 @@
 <?php
+if (class_exists('Mage_Catalog_Model_Resource_Product_Attribute_Collection')) {
 
-class TM_EasyTabs_Model_Product_Attribute_Collection extends Mage_Catalog_Model_Resource_Product_Attribute_Collection
-{
-    protected function _toOptionArray($valueField = 'attribute_code', $labelField = 'frontend_label', $additional = array())
+    class TM_EasyTabs_Model_Product_Attribute_Collection  extends Mage_Catalog_Model_Resource_Product_Attribute_Collection
     {
-        $this->addVisibleFilter();
-        return parent::_toOptionArray($valueField, $labelField, $additional);
+        protected function _toOptionArray($valueField = 'attribute_code', $labelField = 'frontend_label', $additional = array())
+        {
+            $this->addVisibleFilter();
+            return parent::_toOptionArray($valueField, $labelField, $additional);
+        }
+    }
+
+} else {
+    class TM_EasyTabs_Model_Product_Attribute_Collection  extends Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Attribute_Collection
+    {
+        protected function _toOptionArray($valueField = 'attribute_code', $labelField = 'frontend_label', $additional = array())
+        {
+            $this->addVisibleFilter();
+            return parent::_toOptionArray($valueField, $labelField, $additional);
+        }
     }
 }

f26ce5c71ad862f6e0b7350b695b287ddf684b4d
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2013-08-05 10:03:10 +0300

    Fixed undefined offset warning, when block does not unset any other block

--- app/code/community/TM/EasyTabs/Block/Tabs.php
+++ app/code/community/TM/EasyTabs/Block/Tabs.php
@@ -2,24 +2,19 @@
 
 class TM_EasyTabs_Block_Tabs extends Mage_Catalog_Block_Product_View_Tabs
 {
-
     protected function _getCollection()
     {
         $collection = new TM_EasyTabs_Model_Config_Collection();
-        $storeId = Mage::app()->getStore()->getStoreId();
-//        $websiteId = Mage::app()->getWebsite()->getId();
+        $storeId    = Mage::app()->getStore()->getStoreId();
         return $collection
             ->setOrder('sort_order', Varien_Data_Collection::SORT_ORDER_ASC)
             ->addFieldToFilter('status', array('eq' => 1))
-//            ->addFieldToFilter('website_id', array('in' => array($websiteId, 0)))
-            ->addFieldToFilter('store_id', array('in' => array($storeId, 0)))
-            ;
+            ->addFieldToFilter('store_id', array('in' => array($storeId, 0)));
     }
 
     protected function _prepareLayout()
     {
         foreach ($this->_getCollection() as $tab) {
-
             $this->addTab(
                 $tab->getAlias(),
                 $tab->getTitle(),
@@ -33,14 +28,15 @@ class TM_EasyTabs_Block_Tabs extends Mage_Catalog_Block_Product_View_Tabs
                 );
 
             //remove
-            list($blockName, $alias) = explode('::', $tab->getUnset());
-            $block =  $this->getLayout()->getBlock($blockName);
-            if ($block) {
-                $block->unsetChild($alias);
+            $unset = $tab->getUnset();
+            if (false !== strpos($unset, '::')) {
+                list($blockName, $alias) = explode('::', $unset);
+                $block = $this->getLayout()->getBlock($blockName);
+                if ($block) {
+                    $block->unsetChild($alias);
+                }
             }
         }
-
-
         return parent::_prepareLayout();
     }
 }

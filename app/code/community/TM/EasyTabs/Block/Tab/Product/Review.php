7b851bd00fc970005210139f400b3169fb473642
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2013-08-22 10:17:40 +0300

    Added missing child blocks to review and tags blocks

--- app/code/community/TM/EasyTabs/Block/Tab/Product/Review.php
+++ app/code/community/TM/EasyTabs/Block/Tab/Product/Review.php
@@ -2,4 +2,18 @@
 
 class TM_EasyTabs_Block_Tab_Product_Review extends Mage_Review_Block_Product_View_List
 {
+    protected function _prepareLayout()
+    {
+        $reviewForm = $this->getLayout()->createBlock('review/form', 'product.review.form');
+        if ($reviewForm) {
+            $wrapper = $this->getLayout()
+                ->createBlock('page/html_wrapper', 'product.review.form.fields.before');
+            if ($wrapper) {
+                $wrapper->setMayBeInvisible(1);
+                $reviewForm->setChild('form_fields_before', $wrapper);
+            }
+            $this->setChild('review_form', $reviewForm);
+        }
+        return parent::_prepareLayout();
+    }
 }
--- app/code/community/TM/EasyTabs/Block/Tab/Product/Tags.php
+++ app/code/community/TM/EasyTabs/Block/Tab/Product/Tags.php
@@ -2,4 +2,12 @@
 
 class TM_EasyTabs_Block_Tab_Product_Tags extends Mage_Tag_Block_Product_List
 {
+    protected function _prepareLayout()
+    {
+        $wrapper = $this->getLayout()->getBlock('product.tag.list.list.before');
+        if ($wrapper) {
+            $this->setChild('list_before', $wrapper);
+        }
+        return parent::_prepareLayout();
+    }
 }

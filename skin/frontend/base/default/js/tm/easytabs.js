b5c860b11bdaff3c4aaa95a256a88c5475868598
0m3r <0m3r.mail@gmail.com>
2013-10-22 16:18:01 +0300

    anchor support was added

---
+++ skin/frontend/base/default/js/tm/easytabs.js
@@ -0,0 +1,50 @@
+var EasyTabs = Class.create();
+EasyTabs.prototype = {
+    initialize: function(selector) {
+        $$(selector).each(this.initTab.bind(this));
+        this.showContent(window.location.hash);
+        var contents = $$(window.location.hash + '_contents').first();
+        if (contents) {
+            contents.scrollTo();
+        } else {
+
+        }
+    },
+
+    initTab: function(el) {
+        if ($(el).up('li').hasClassName('active')) {
+            this.showContent(el);
+        }
+        el.observe('click', this.showContent.bind(this, el));
+    },
+
+    showContent: function(a) {
+        if ('string' == typeof a) {
+            var hash = a.split("#")[1];
+        } else {
+            var hash = a.href.split("#")[1];
+        }
+        if (!hash) {
+            return this;
+        }
+
+        var contentToShow = $(hash + '_contents');
+        if (!contentToShow) {
+            return;
+        }
+        contentToShow.siblings().each(function(contentToHide) {
+            contentToHide.hide();
+            var _hash = contentToHide.id.replace('_contents', ''),
+                a = $$('a[href="#' + _hash + '"]').first();
+            if (a) {
+                a.up('li').removeClassName('active');
+            }
+        });
+        contentToShow.show();
+        a = $$('a[href="#' + hash + '"]').first();
+        if (a) {
+            a.up('li').addClassName('active');
+        }
+        return this;
+    }
+}

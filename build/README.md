f927563e2788e1fa5a94c1867441267b150669cc
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2014-07-08 11:14:15 +0300

    Release script added

---
+++ build/README.md
@@ -0,0 +1,7 @@
+# How to make release version
+1. Install [node.js](http://nodejs.org/).
+2. Run `npm install -g gulp` if you didn't run it before.
+3. Run `npm install` command inside build folder.
+4. Run `gulp` command from terminal.
+
+That's all. Archive with latest module version will be created and saved to bin folder.
---
+++ build/gulpfile.js
@@ -0,0 +1,65 @@
+var gulp = require('gulp'),
+    zip  = require('gulp-zip'),
+    size = require('gulp-filesize'),
+    exec = require('child_process').exec,
+    fs   = require('fs');
+
+var tm = (function() {
+    var moduleInfo = {};
+
+    function loadModuleInfo() {
+        var data = fs.readFileSync('../composer.json', {
+            encoding: 'utf8'
+        });
+
+        moduleInfo = JSON.parse(data);
+        moduleInfo.shortname = moduleInfo.name.replace('tm/', '');
+    }
+
+    return {
+        getModuleInfo: function() {
+            if (!moduleInfo.name) {
+                loadModuleInfo();
+            }
+            return moduleInfo;
+        },
+        getArchiveName: function(suffix) {
+            suffix = suffix || '';
+            return [
+                this.getModuleInfo().shortname,
+                '-',
+                this.getModuleInfo().version,
+                suffix,
+                '.zip'
+            ].join('');
+        }
+    };
+}());
+
+gulp.task('composer', function(cb) {
+    var folders = ['vendor', 'code'];
+    folders.forEach(function(folder) {
+        if (!fs.existsSync(folder)) {
+            fs.mkdirSync(folder, function(e) { console.log(e); });
+        }
+    });
+
+    fs.exists('composer.lock', function(exists) {
+        var cmd = exists ? 'composer update' : 'composer install';
+        console.log(cmd + ' is running');
+        exec(cmd, function (err, stdout, stderr) {
+            console.log(stdout);
+            console.log(stderr);
+            cb(err);
+        });
+    });
+});
+
+gulp.task('full', ['composer'], function() {
+    gulp.src(['code/**/*'])
+        .pipe(zip(tm.getArchiveName()))
+        .pipe(size())
+        .pipe(gulp.dest('bin'));
+});
+
+gulp.task('default', ['full']);

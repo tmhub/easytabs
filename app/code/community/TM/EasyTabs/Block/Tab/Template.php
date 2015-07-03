c18aff9eefcbde4e4d7a0144f20bdc7803ff9b06
Alexander Kras'ko <0m3r.mail@gmail.com>
2015-07-03 12:36:51 +0300

    getCount& getTabBlock was added on tab template block

--- app/code/community/TM/EasyTabs/Block/Tab/Template.php
+++ app/code/community/TM/EasyTabs/Block/Tab/Template.php
@@ -15,11 +15,25 @@ class TM_EasyTabs_Block_Tab_Template extends Mage_Core_Block_Template
 
     protected function _toHtml()
     {
-        $block = $this->getChild($this->getNameInLayout() . '_tab');
+        $block = $this->getTabBlock();
         if ($block instanceof Mage_Core_Block_Template) {
             return $block->setTemplate($this->getTemplate())
                 ->toHtml();
         }
         return '';
+    }
+
+    public function getTabBlock()
+    {
+        return $this->getChild($this->getNameInLayout() . '_tab');
+    }
+
+    public function getCount()
+    {
+        $block = $this->getTabBlock();
+        if ($block instanceof Mage_Core_Block_Template) {
+            return $block->getCount();
+        }
+        return;
     }
 }

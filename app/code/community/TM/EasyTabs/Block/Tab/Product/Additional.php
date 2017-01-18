b194553374a65c26309f3c4b9ea803702f347a72
Vitaliy <vitaliy@mage2cloud.com>
2017-01-18 17:59:12 +0200

    config option 'group attributes'

--- app/code/community/TM/EasyTabs/Block/Tab/Product/Additional.php
+++ app/code/community/TM/EasyTabs/Block/Tab/Product/Additional.php
@@ -1,5 +1,16 @@
 <?php
 
-class TM_EasyTabs_Block_Tab_Product_Additional extends Mage_Catalog_Block_Product_View_Attributes
+class TM_EasyTabs_Block_Tab_Product_Additional
+    extends Mage_Catalog_Block_Product_View_Attributes
 {
+
+    protected function _construct()
+    {
+        parent::_construct();
+        $this->setData(
+            'is_grouped',
+            Mage::getStoreConfig('tm_easytabs/general/group_attributes')
+        );
+    }
+
 }

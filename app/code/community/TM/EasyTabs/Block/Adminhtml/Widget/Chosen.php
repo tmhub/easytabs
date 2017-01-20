5af44e7bd41b7af80def73f9c3042f9c75f9aff1
Vitaliy <vitaliy@mage2cloud.com>
2017-01-20 12:19:30 +0200

    widget backend interface  (overlow:visible for parents via js)

--- app/code/community/TM/EasyTabs/Block/Adminhtml/Widget/Chosen.php
+++ app/code/community/TM/EasyTabs/Block/Adminhtml/Widget/Chosen.php
@@ -12,15 +12,23 @@ class TM_EasyTabs_Block_Adminhtml_Widget_Chosen
      */
     public function prepareElementHtml(Varien_Data_Form_Element_Abstract $element)
     {
-        $html = '<script type="text/javascript">'
-            . 'new Chosen('
-            .  '$("' . $element->getId() . '"),'
-            .  '{width: "280px", placeholder_text: "' . $this->__('Select Tabs') . '"}'
-            . ');'
-            . '$("'. $element->getId() .'")'
-            . '.up(".entry-edit .fieldset .hor-scroll")'
-            . '.setStyle({overflow: "visible"});'
-            .'</script>';
+        $html = <<<HTML
+<script type="text/javascript">
+    new Chosen(
+        $("{$element->getId()}"),
+        {width: "280px", placeholder_text: "{$this->__('Select Tabs')}"});
+    $("{$element->getId()}")
+        .up(".entry-edit .fieldset .hor-scroll")
+        .setStyle({overflow: "visible"});
+    if ($("{$element->getId()}").up("#widget_window_content")) {
+        $("{$element->getId()}")
+            .up("#widget_window_content")
+            .setStyle({overflow: "visible"})
+            .up("#widget_window_table_content")
+            .setStyle({overflow: "visible"});
+    }
+</script>
+HTML;
         $element->setData('after_element_html', $html);
         return $element;
     }

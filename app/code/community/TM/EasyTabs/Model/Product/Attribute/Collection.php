b1fb9415ff2cf9de0faf578a85573f147e7852e9
Alexander Kras'ko <0m3r.mail@gmail.com>
2017-08-23 17:50:57 +0300

    Fix MEQP.1. Each class must be in a file by itself

--- app/code/community/TM/EasyTabs/Model/Product/Attribute/Collection.php
+++ app/code/community/TM/EasyTabs/Model/Product/Attribute/Collection.php
@@ -1,22 +1,10 @@
 <?php
-if (class_exists('Mage_Catalog_Model_Resource_Product_Attribute_Collection')) {
 
-    class TM_EasyTabs_Model_Product_Attribute_Collection  extends Mage_Catalog_Model_Resource_Product_Attribute_Collection
+class TM_EasyTabs_Model_Product_Attribute_Collection extends Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Attribute_Collection
+{
+    protected function _toOptionArray($valueField = 'attribute_code', $labelField = 'frontend_label', $additional = array())
     {
-        protected function _toOptionArray($valueField = 'attribute_code', $labelField = 'frontend_label', $additional = array())
-        {
-            $this->addVisibleFilter();
-            return parent::_toOptionArray($valueField, $labelField, $additional);
-        }
-    }
-
-} else {
-    class TM_EasyTabs_Model_Product_Attribute_Collection  extends Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Attribute_Collection
-    {
-        protected function _toOptionArray($valueField = 'attribute_code', $labelField = 'frontend_label', $additional = array())
-        {
-            $this->addVisibleFilter();
-            return parent::_toOptionArray($valueField, $labelField, $additional);
-        }
+        $this->addVisibleFilter();
+        return parent::_toOptionArray($valueField, $labelField, $additional);
     }
 }
